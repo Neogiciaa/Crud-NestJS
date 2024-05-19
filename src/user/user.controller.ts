191d999c7c1395ddc3f3e6e133f360c3cdbe9893
Neogiciaa <chevalier.stephen@hotmail.com>
2024-05-19 19:38:03 +0200

    Added findAll method to userController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,4 +4,9 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
 }
