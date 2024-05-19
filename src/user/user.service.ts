7a3e6203997129dc8d0e8e724d21b009c029ec91
Neogiciaa <chevalier.stephen@hotmail.com>
2024-05-19 19:44:11 +0200

    Added Users[] to simulate database table

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import User from './user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  users: User[] = [
+    {
+      id: 1,
+      email: 'Test@test.be',
+      password: 'TestPassword',
+      firstName: 'Stéphen',
+      lastName: 'Chevalier',
+    },
+    {
+      id: 2,
+      email: 'Test2@test.be',
+      password: 'TestPassword',
+      firstName: 'Neo',
+      lastName: 'Giciaa',
+    },
+  ];
+}
